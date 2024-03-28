ad533fd295883706276a6fa3124ef4ba0cc8af73
Instmois <nikita-moiseev-2003@ya.ru>
2024-03-29 02:11:56 +0300

    1.0 version

--- src/main/java/com/example/Neoflex/VacationCalculatorController.java
+++ src/main/java/com/example/Neoflex/VacationCalculatorController.java
@@ -1,14 +1,13 @@
 package com.example.Neoflex;
 
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class VacationCalculatorController {
 
-    @Autowired
     private final VacationCalculatorService vacationCalculatorService;
 
     public VacationCalculatorController(VacationCalculatorService vacationCalculator) {
@@ -16,8 +15,8 @@ public class VacationCalculatorController {
     }
 
     @GetMapping("/calculate")
-    public VacationResponse calculateVacationPay(@RequestParam(required = true) @Min(0) double averageSalary,
-                                                 @RequestParam(required = true) @Min(0) @Max(31) int vacationDays) {
+    public VacationResponse calculateVacationPay(@Valid @RequestParam @Min(0) double averageSalary,
+                                                 @Valid @RequestParam @Min(0) @Max(31) int vacationDays) {
         double vacationPay = vacationCalculatorService.calculateVacationPay(averageSalary, vacationDays);
         return new VacationResponse(vacationPay);
     }
